e6bbe6c6ecafd753823a77b972c3d748f9a53605
w1ll1ann <willEarth255@gmail.com>
2023-11-03 14:16:30 -0300

    Define the revealCell method and tweak the draw method

--- src/minesweeper/board.cpp
+++ src/minesweeper/board.cpp
@@ -2,51 +2,113 @@
 
 using namespace Minesweeper;
 
-const std::string redText {"\033[91m"};
-const std::string greyText {"\033[90m"};
-const std::string yellowText {"\033[93m"};
-const std::string defaultText {"\033[m"};
-
 Board::Board() {
     Board({8, 8}, 10);
 }
 
 Board::Board(std::pair <int, int> gridSize, int numberOfMines) {
+    totalCells = gridSize.first * gridSize.second;
+
     setGrid(gridSize);
     generateMines(numberOfMines);
     setNearbyMinesCellsValues();
+    setOutOfBoundsCells();
 }
 
 void Board::draw() {
+    std::cout << "MS ";
+    for (unsigned int i = 0; i < grid[0].size() - 2; i++)
+        if (i < 10)
+            std::cout << "0" + std::to_string(i) << " ";
+        else
+            std::cout << i << " ";
+    std::cout << "J\n";
+
     for (unsigned int i = 1; i < grid.size() - 1; i++) {
+        if (i < 10)
+            std::cout << "0" + std::to_string(i - 1) << "  ";
+        else
+            std::cout << i << "  ";
+
         for (unsigned int j = 1; j < grid[i].size() - 1; j++)
             if (!grid[i][j].isRevealed)
-                std::cout << "- ";
+                std::cout << "-" << "  ";
+            else if (grid[i][j].isMine)
+                std::cout << redText << "X" << defaultText << "  ";
             else if (grid[i][j].nearbyMines == 0)
-                std::cout << greyText << grid[i][j].nearbyMines << defaultText << " ";
+                std::cout << greyText << grid[i][j].nearbyMines << defaultText << "  ";
             else
-                std::cout << yellowText << grid[i][j].nearbyMines << defaultText << " ";
+                std::cout << yellowText << grid[i][j].nearbyMines << defaultText << "  ";
         std::cout << "\n";
     }
+
+    std::cout << " I\n";
 }
 
-void Board::drawAll() {
-    for (unsigned int i = 1; i < grid.size() - 1; i++) {
-        for (unsigned int j = 1; j < grid[i].size() - 1; j++)
-            if (grid[i][j].isMine)
-                std::cout << redText << "X " << defaultText;
-            else if (grid[i][j].nearbyMines == 0)
-                std::cout << greyText << grid[i][j].nearbyMines << defaultText << " ";
-            else
-                std::cout << yellowText << grid[i][j].nearbyMines << defaultText << " ";
-        std::cout << "\n";
+void Board::revealAll() {
+    for (unsigned int i = 0; i < grid.size(); i++)
+        for (unsigned int j = 0; j < grid[i].size(); j++)
+            grid[i][j].isRevealed = true;
+}
+
+void Board::revealMines() {
+    for (auto i : minesPositions)
+        grid[i.first][i.second].isRevealed = true;
+}
+
+int Board::revealCell(std::pair <int, int> cellPosition) {
+    if (grid[cellPosition.first][cellPosition.second].isRevealed)
+        return 0;
+
+    grid[cellPosition.first][cellPosition.second].isRevealed = true;
+    revealedCells++;
+
+    if (grid[cellPosition.first][cellPosition.second].isMine) {
+        hasMineBeenFound = true;
+        return 0;
     }
+
+    int accumulatedScore {0};
+    if (grid[cellPosition.first][cellPosition.second].nearbyMines == 0) {
+        for (int i = -1; i <= 1; i++)
+            for (int j = -1; j <=1; j++)
+                if (grid[cellPosition.first + i][cellPosition.second + j].isMine)
+                    continue;
+                else
+                    accumulatedScore += revealCell({cellPosition.first + i, cellPosition.second + j});
+    }
+    else
+        accumulatedScore = grid[cellPosition.first][cellPosition.second].nearbyMines * 100;
+
+    return accumulatedScore;
+}
+
+bool Board::hasCellBeenRevealed(std::pair <int, int> cellPosition) {
+    return grid[cellPosition.first][cellPosition.second].isRevealed;
+}
+
+bool Board::hasAllCellsBeenRevealed() {
+    return (unsigned)revealedCells == (totalCells - minesPositions.size()); 
 }
 
 void Board::setGrid(std::pair <int, int> gridSize) {
     grid.resize(gridSize.first + 2);
     for (unsigned int i = 0; i < grid.size(); i++)
         grid[i].resize(gridSize.second + 2);
+}
+
+void Board::setOutOfBoundsCells() {
+    for (unsigned int i = 0; i < grid[0].size(); i++)
+        grid[0][i].isRevealed = true;
+
+    for (unsigned int i = 0; i < grid[0].size(); i++)
+        grid[grid.size() - 1][i].isRevealed = true;
+
+    for(unsigned int i = 0; i < grid.size(); i++)
+        grid[i][0].isRevealed = true;
+
+    for(unsigned int i = 0; i < grid.size(); i++)
+        grid[i][grid[0].size() - 1].isRevealed = true;
 }
 
 void Board::generateMines(int numberOfMines) {
--- src/minesweeper/headers/board.h
+++ src/minesweeper/headers/board.h
@@ -7,6 +7,12 @@
 #include <iostream>
 #include <algorithm>
 
+const std::string redText {"\033[91m"};
+const std::string greyText {"\033[90m"};
+const std::string greenText {"\033[92m"};
+const std::string yellowText {"\033[93m"};
+const std::string defaultText {"\033[m"};
+
 namespace Minesweeper {
     struct Cell {
         bool isRevealed {false};
@@ -21,12 +27,26 @@ namespace Minesweeper {
             Board(std::pair <int, int> gridSize, int numberOfMines);
 
             void draw();
-            void drawAll();
+
+            void revealAll();
+            void revealMines();
+
+            int revealCell(std::pair <int, int> cellPosition);
+
+            bool hasCellBeenRevealed(std::pair <int, int> cellPosition);
+            bool hasAllCellsBeenRevealed();
+
         private:
+            int revealedCells {0};
+            int totalCells {0};
+
+            bool hasMineBeenFound {false};
+
             std::vector <std::pair<int, int>> minesPositions;
             std::vector <std::vector <Cell>> grid;
 
             void setGrid(std::pair <int, int> gridSize);
+            void setOutOfBoundsCells();
             void generateMines(int numberOfMines);
             void setNearbyMinesCellsValues();
 
