6e9560b7eb06c79630cd7807fb22753c986065e8
Luana Oliveira <79430579+SilikaLin@users.noreply.github.com>
2023-11-03 22:32:48 -0300

    Extração da estrutura cell de board.h para cell.h
    A definição da estrutura Cell foi movida para seu próprio arquivo de cabeçalho para melhorar a modularidade do código e a separação de interesses. Esta mudança promove melhor capacidade de reutilização e manutenção da estrutura da célula em todo o projeto.

---
+++ board.h
@@ -0,0 +1,48 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#include "cell.h"
+
+namespace Minesweeper
+{
+    class Board
+    {
+        public:
+            Board();
+            Board(const std::pair <int, int> &gridSize, int numberOfMines);
+
+            void draw();
+
+            void revealAll();
+            void revealMines();
+
+            int revealCell(const std::pair <int, int> &cellPosition);
+
+            bool hasCellBeenRevealed(const std::pair <int, int> &cellPosition);
+            bool hasAllCellsBeenRevealed();
+
+            bool hasMineBeenFound();
+
+            std::pair <int, int> getGridDimensions();
+
+        private:
+            int revealedCells {0};
+            int totalCells {0};
+
+            bool aMineHasBeenFound {false};
+
+            std::vector <std::pair<int, int>> minesPositions;
+            std::vector <std::vector <Cell>> grid;
+
+            void drawGridNumbers(int number);
+
+            void setGrid(const std::pair <int, int> &gridSize);
+            void setOutOfBoundsCells();
+            void generateMines(int numberOfMines);
+            void setNearbyMinesCellsValues();
+
+            int countNearbyMines(const std::pair<int, int> &cellPosition);
+    };
+}
+
+#endif
